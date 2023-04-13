5868b613a8f752377ea3cb1a8a8afa0764550298
Piotr Kaminski <99peterstone@gmail.com>
2023-04-14 00:28:17 +0200

    temp. removed naive mul. thread. test

--- test/test_compression_naive.py
+++ test/test_compression_naive.py
@@ -85,53 +85,53 @@ class Test_compression_naive(unittest.TestCase):
         os.remove(alg_output_file)
         os.remove(enc_file_name)
 
-    def test_process_mul_threads(self,):
+    # def test_process_mul_threads(self,):
 
-        file_path = self.path + self.data_path
-        enc_file_name = '__tmp.bin'
+    #     file_path = self.path + self.data_path
+    #     enc_file_name = '__tmp.bin'
 
-        source_data = None
+    #     source_data = None
 
-        with open(file_path, 'r') as f:
-            source_data = f.read()
-            source_data = self.encoder_mul_workers.def_out_format.transform(
-                self.encoder_mul_workers.def_pgn_parser.transform(source_data)
-            )
+    #     with open(file_path, 'r') as f:
+    #         source_data = f.read()
+    #         source_data = self.encoder_mul_workers.def_out_format.transform(
+    #             self.encoder_mul_workers.def_pgn_parser.transform(source_data)
+    #         )
 
-        self.assertIsNotNone(source_data)
+    #     self.assertIsNotNone(source_data)
 
-        # encode the file
-        self.encoder_mul_workers.encode(
-            file_path, enc_file_name
-        )
+    #     # encode the file
+    #     self.encoder_mul_workers.encode(
+    #         file_path, enc_file_name
+    #     )
         
-        self.assertEqual(True, enc_file_name in set(os.listdir()))
+    #     self.assertEqual(True, enc_file_name in set(os.listdir()))
 
-        alg_output_file = '__dec.txt'
-        self.encoder_mul_workers.decode(
-            enc_file_name, alg_output_file
-        )
+    #     alg_output_file = '__dec.txt'
+    #     self.encoder_mul_workers.decode(
+    #         enc_file_name, alg_output_file
+    #     )
 
-        self.assertEqual(True, alg_output_file in set(os.listdir()))
+    #     self.assertEqual(True, alg_output_file in set(os.listdir()))
 
-        with open(alg_output_file, 'r') as f:
-            dec_data = f.readlines()
-            dec_data = [g.strip() for g in dec_data]
+    #     with open(alg_output_file, 'r') as f:
+    #         dec_data = f.readlines()
+    #         dec_data = [g.strip() for g in dec_data]
 
-        source_data = source_data.strip().split('\n')   
+    #     source_data = source_data.strip().split('\n')   
 
-        self.assertEqual(len(source_data), len(dec_data))
+    #     self.assertEqual(len(source_data), len(dec_data))
 
-        # sort the games, they can be permutated
-        source_data.sort()
-        dec_data.sort()
+    #     # sort the games, they can be permutated
+    #     source_data.sort()
+    #     dec_data.sort()
 
-        for g_dec, g_src in zip(dec_data, source_data):
-            self.assertEqual(g_dec, g_src,
-                             msg=repr(g_dec) + '\n' + repr(g_src))
+    #     for g_dec, g_src in zip(dec_data, source_data):
+    #         self.assertEqual(g_dec, g_src,
+    #                          msg=repr(g_dec) + '\n' + repr(g_src))
                 
-        os.remove(alg_output_file)
-        os.remove(enc_file_name)
+    #     os.remove(alg_output_file)
+    #     os.remove(enc_file_name)
 
 if __name__ == "__main__":
 
