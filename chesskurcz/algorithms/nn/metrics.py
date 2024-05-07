2a054c8c0b90001af06f94e25b177e3da5e27458
Piotr Kaminski <99peterstone@gmail.com>
2024-05-07 18:13:56 +0200

    prototype of CompressionMetric

--- chesskurcz/algorithms/nn/metrics.py
+++ chesskurcz/algorithms/nn/metrics.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import numpy as np
+from typing import List, Dict, Any, Tuple
 
 import chess
 
@@ -11,21 +13,50 @@ class CompressionMetric:
     """
     def __init__(self,):
 
+        self.max = {}
         self.sums = {}
-        self.count = {}
+        self.count_per_game = {}
+        self.count = 0
 
-    def update(self, game_num: torch.Tensor, 
-               fen: torch.Tensor, move_uci: torch.Tensor, 
-               data: torch.Tensor, targets: torch.Tensor):
+    def update(self, game_nums: List[int], 
+               fen: List[str], move_uci: torch.Tensor, 
+               predicted: torch.Tensor, targets: torch.Tensor):
 
+        metric = []
+        for i, game_num, position, move in enumerate(zip(game_nums, fen, move_uci)):
+            if game_num not in self.max.keys():
+                self.max[game_num] = 0
+                self.sums[game_num] = 0
+                self.count_per_game[game_num] = 0 
+
+            metric_val = self.calculate(position, move, predicted[i], targets[i])
+            self.max[game_num] = max(self.max[game_num], metric_val)
+            self.sums[game_num] += metric_val
+            self.count_per_game[game_num] += 1
+            self.count += 1
+
+    def calculate(self, fen: str, move_uci: str,
+                  predicted: torch.Tensor, target: torch.Tensor) -> int:
+
+        position = chess.Board(fen)
+        move = chess.Move.from_uci(move_uci)
+
+        rank = self.__calculate_move_rank(position, predicted, target)
+
+        return  np.floor(np.log2(rank)) + 1
+    
+    def __calculate_move_rank(self, position: chess.Board,
+                              predicted: torch.Tensor, target: torch.Tensor) -> int:
+        
+        # TODO
         pass
 
-    def calculate(self, fen: torch.Tensor, move_uci: torch.Tensor,
-                  data: torch.Tensor, targets: torch.Tensor):
+    def result(self,) -> Tuple[float, float]: 
 
-        positions = [chess.Board(f) for f in fen]
-        pass
+        if not self.count:
+            return None, None 
 
-    def result(self,): 
+        avg_max_bits = np.sum(list(self.max.values()), dtype=np.float32) / len(self.count_per_game)
+        avg_agv_bits = np.sum(list(self.sums.values()), dtype=np.float32) / self.count
 
-        pass
+        return avg_max_bits, avg_agv_bits
